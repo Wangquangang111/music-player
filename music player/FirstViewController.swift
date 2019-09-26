a9ea4d901ff173471584847343d3d3a96c5de49e
王泉钢 <tcfgarywang@gmail.com>
2019-09-27 06:41:06 +1200

    Initial Commit

---
+++ music player/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  music player
+//
+//  Created by AKLZZ on 27/09/19.
+//  Copyright © 2019 Quangangwang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ music player/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  music player
+//
+//  Created by AKLZZ on 27/09/19.
+//  Copyright © 2019 Quangangwang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
